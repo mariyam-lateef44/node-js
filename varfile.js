4670cf4a6543ebd9f0b927551a596816e974d8ff
mariyam-lateef44 <163380334+mariyam-lateef44@users.noreply.github.com>
2024-05-09 21:31:46 +0500

    Add files via upload

---
+++ varfile.js
@@ -0,0 +1,9 @@
+var name="mariyam";
+let age = 10;
+const salary = 100;
+//let age =01; we can't use it again
+//salary= 10;  we can;t modify it and can't use it
+var name= "Muhammad"; // we can use it again and also modify it
+var num1 =100;
+var num2 = 2.5;
+console.log(name + num1 +  num2);
